0293af9409f44795d9bc42b7dbbbe9922785e813
dasoya <66422476+dasoya@users.noreply.github.com>
2022-09-26 16:01:49 +0200

    [맵과 셋] - Update (2)

--- 09월 06일 - 맵과 셋/2910.cpp
+++ 09월 06일 - 맵과 셋/2910.cpp
@@ -7,15 +7,14 @@ using namespace std;
 map<int,int> index_map;
 bool cmp(const pair<int,int>& A, const pair<int,int>& B){
 
-    if(A.first == B.first){
-
-        return index_map[A.second] < index_map[B.second]; // 빈도가 같으면 먼저 나온 게 출력
+    if(A.first != B.first){
+        return A.first > B.first;
+        // 빈도 높은 순
     }
-    return A.first > B.first; // 빈도 높은 순
+    return index_map[A.second] < index_map[B.second];
 }
 int main(){
     int n,c;
-    vector<pair<int,int>> arr;
     map<int,int> quan;
     cin >> n >> c;
 
@@ -30,7 +29,9 @@ int main(){
         }
     }
 
-    for(auto& iter : quan){
+    vector<pair<int,int>> arr;
+
+    for(auto &iter : quan){
         arr.emplace_back(iter.second,iter.first);
     }
 
