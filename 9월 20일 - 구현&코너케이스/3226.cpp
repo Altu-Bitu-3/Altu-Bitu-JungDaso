97bc4b90feb5c4647e71e008154c50dae92808bd
dasoya <66422476+dasoya@users.noreply.github.com>
2022-10-02 00:05:01 +0200

    [구현&코너 케이스] - 10월 2일

--- 9월 20일 - 구현&코너케이스/3226.cpp
+++ 9월 20일 - 구현&코너케이스/3226.cpp
@@ -11,34 +11,29 @@ int calc(string input,int k){
     //통화가 끝나는 시간
     int e_min = (min +k)%60;
     int e_hour = (hour + (min+k)/60)%24;
-
-    int fee = 0;
+    
 
     if(hour < 7 && e_hour >= 7){ // 통화 중간에 7:00시를 지나는 경우
 
-        fee += (60-min)* 5;
-        fee+= e_min*10;
-        return fee;
+        return  (60-min)* 5 + e_min*10;
+
     }
 
     if(hour<19 && e_hour >= 19 ) // 통화 중간에 19:00시를 지나는 경우
     {
-        fee += (60-min)* 10;
-        fee+= e_min*5;
-        return fee;
+        return (60-min)* 10 + e_min*5;
+
     }
 
     // 같은 요금제 시간대 에서 통화가 끝나는 경우
     if(7 <= hour && 19 > hour){ // 7:00 ~ 18:59분
-        fee += k*10;
-
-    }
-    else{
-        fee += k*5;
+        return k*10;
 
     }
 
-    return fee;
+
+        return  k*5;
+
 
 
 }
